8147bb348da4157ab9c4b71d4c81679dc51e1d6e
David <kerepakupai@outlook.com>
2020-03-15 22:25:27 -0300

    first commit

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    A page where we can create an event.
+  </h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ src/views/NotFoundComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    Not Found Components Please Review
+  </h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
